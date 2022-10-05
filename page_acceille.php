981edda890ecc2d2b7419bc337c072fccda6319a
sen2019elec <sen.elecplomb2019@gmail.com>
2022-10-05 23:25:52 +0200

    valide

---
+++ page_acceille.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link href="page_accueille.css" rel="stylesheet">
+   
+   
+</head>
+<body>
+<img src="gfg.png" data-toggle="modal"
+        data-target="#exampleModal">
+    
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+  <button class="btn btn-outline-success" type="submit">Accueille</button>
+    <button class="navbar-toggler" type="button"
+     data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul><div>
+
+        </div>
+        </ul>
+        <button class="btn btn-outline-success" type="submit">Actualité</button>
+          <ul class="dropdown-menu">
+           
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <ul>
+            <div>
+            </div>
+        </ul>
+        
+        <button class="btn btn-outline-success" type="submit">Admissiblité</button>
+        <ul><div></div></ul>
+      </ul>
+      <form class="d-flex" role="search">
+      <button class="btn btn-outline-success" type="submit">Inscription</button>
+      <ul><div></div></ul>
+        <button class="btn btn-outline-success" type="submit">Historique</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="bg-image" 
+     style="background-image: url('pexels-roberto-nickson-2559941-1.jpg');
+            height: 100vh">
+
+<div class="description">
+    <div><ul>
+        
+    <h3><i class="fa-sharp fa-solid fa-graduation-cap"></i> ecole privé</h3>  
+           <h1> LA REUSSIE</h1>
+    
+    </ul>
+    <ul>
+        
+           Enségnement Général :   <h2>Primaire & Secondaire </h2>
+        
+        <p>serie :  S/L De la <h3>CP à la TÉRMINALE</h3></p>
+    </ul>
+    <ul>
+        
+           <h3><i class="fa-sharp fa-solid fa-location-dot"></i>  Keur Mbaye Fall </h3>
+      
+        
+       <h3><i class="fa-sharp fa-solid fa-envelope-circle-check"></i>  ecol22@gmail.com </h3> 
+        
+        <h3>
+        <i class="fa-sharp fa-solid fa-phone-volume"></i> 33 445 44 44</h3>
+        
+    </ul>
+    <ul>
+    <h3><i class="fa-sharp fa-solid fa-book-bookmark"></i>  la culte de l'excélance</h3>
+    </ul>
+  
+    </div>
+   <!-- <div class="bg-image" 
+     style="background-image: url('https://www.codeur.com/tuto/wp-content/uploads/2022/01/pexels-roberto-nickson-2559941-1.jpg');
+            height: 100vh">
+</div> -->
+  
+    
+</div>
+</div>          
+
+
+
+            
+            
+</div> 
+</body>
+</html>
